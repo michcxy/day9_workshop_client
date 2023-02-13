1a0ad81cd904c6dfacb825e6dea9d55234370ce0
mich <mich.cxy@gmail.com>
2023-02-13 11:33:08 +0800

    initial commit

---
+++ src/main/java/batch3/App.java
@@ -0,0 +1,11 @@
+package batch3;
+
+//THIS IS THE CLIENT
+public final class App {
+    private App() {
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/test/java/batch3/AppTest.java
@@ -0,0 +1,18 @@
+package batch3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
